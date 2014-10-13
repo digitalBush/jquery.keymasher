f87ef1a9d367d401d2f91c4be8384e98c16a50d7
Josh Bush <josh@digitalbush.com>
2014-10-12 22:37:35 -0500

    Adding readonly check

--- src/jquery.keymasher.js
+++ src/jquery.keymasher.js
@@ -1,13 +1,13 @@
 /*
 	Key Masher plugin for jQuery (https://github.com/digitalBush/jquery.keymasher)
-	Copyright (c) 2010-2013 Josh Bush (digitalbush.com)
+	Copyright (c) 2010-2014 Josh Bush (digitalbush.com)
 	Licensed under the MIT license
-	Version: 0.3
+	Version: 0.4
 */
 
 (function($,undefined){
 		//numberPad={'0':96,'1':97,'2':98,'3':99,'4':100,'5':101,'6':102,'7':103,'8':104,'9':105,'*':106,'+':107,'-':109,'.':110,'/':111},
-	
+
 	var keys=(function(){
 		var	defs={},
 			keys    = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`-=[]\\;',./ \t\n\r",
@@ -17,7 +17,7 @@
 					insert:45, 'delete':46,pause:19,esc:27,left:37,up:38,right:39,down:40,printscreen:44};
 
 		$.each(keys.split(''),function(index,value){
-			var keyCode=value.charCodeAt(0),shift=shifted[index];			
+			var keyCode=value.charCodeAt(0),shift=shifted[index];
 			defs[value]={keyCode:keyCode,charCode:keyCode,shift:shift};
 			if(shift)
 				defs[shift]={keyCode:keyCode,charCode:shift.charCodeAt(0),shift:value,requiresShift:index>=26};
@@ -29,39 +29,39 @@
 	var KeyMasher=function(elm){
 		var	modifierState={alt: false, ctrl: false,	meta: false, shift: false},
 			forced={};
-			
+
 		var queueModifierEvent=function(direction,modifier,isForced){
 			forced[modifier]=isForced;
-			modifierState[modifier]=(direction=='down');		
+			modifierState[modifier]=(direction=='down');
 			var event=$.extend($.Event(), modifierState, {type:'key'+direction, keyCode: keys[modifier].keyCode, charCode: 0});
 			elm.trigger(event);
 		};
-		
+
 		var queueStroke=function(key){
 			if($.type(key)==='string')
-				key=keys[key];	
+				key=keys[key];
 			if(key.requiresShift && !modifierState.shift)
 				queueModifierEvent('down','shift',true);
 			else if(modifierState.shift && key.shift)
 				key=keys[key.shift];
-			
+
 			var ignore = !key.charCode || modifierState.alt || modifierState.ctrl || modifierState.meta,
 				down = $.extend($.Event('keydown'), modifierState, {keyCode: key.keyCode, charCode: 0, which:key.keyCode}),
 				press = $.extend($.Event('keypress'), modifierState, {keyCode: key.charCode, charCode: key.charCode, which: key.charCode}),
-				up = $.extend($.Event('keyup'), modifierState, {keyCode: key.keyCode, charCode: 0, which:key.keyCode});			
-			
+				up = $.extend($.Event('keyup'), modifierState, {keyCode: key.keyCode, charCode: 0, which:key.keyCode});
+
 			elm.trigger(down);
 			if(!down.isDefaultPrevented() && !ignore){
 				elm.trigger(press);
-				if(!press.isDefaultPrevented()){					
+				if(!press.isDefaultPrevented() && !elm.prop("readonly")){
 					//need to do caret positioning
 					elm.val(elm.val()+String.fromCharCode(key.charCode));
 				}
 			}
 			elm.trigger(up);
-			
+
 			if(forced.shift)
-				queueModifierEvent('up','shift');			
+				queueModifierEvent('up','shift');
 		};
 
 		var public={
@@ -71,13 +71,13 @@
 				public.type(typing);
 				$.each(toks,function(index,value){queueModifierEvent('up',value);});
 				return public;
-			},				
+			},
 			type:function(){
 				$.each(arguments,function(index,typing){
 					if($.type(typing)==='string')
 						$.each(typing.split(''),function(index,value){queueStroke(value);});
 					else
-						queueStroke(typing);					
+						queueStroke(typing);
 				});
 				return public;
 			}
@@ -91,7 +91,7 @@
 			fn=function(keys){keys.type(typing)};
 		}
 		return this.each(function(){
-			fn(KeyMasher($(this)));			
+			fn(KeyMasher($(this)));
 		});
 	};
 })(jQuery);
